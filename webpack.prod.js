78b00ce0a4feaf1537ca13e3a41047cbfd834734
LaurenMartini <laurenmartini55@gmail.com>
2021-05-11 22:32:43 -0700

    chore: updated plugins and finalized production
    Added MiniCssExtractPlugin, OptimizeCSSAssetsPlugin, and TerserPlugin to prod
    Added webpack service worker for offline functionality

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -14,7 +18,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
     },
@@ -22,6 +26,11 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
-    ]
+        }),
+        new WorkboxPlugin.GenerateSW(),
+        new MiniCssExtractPlugin({filename: "[name].css"})
+    ],
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    }
 }
